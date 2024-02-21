4e20b0241b1494191f96267be9097fee4602fda0
Sachinsingh1608 <sk379403@gmail.com>
2024-02-21 20:49:12 +0530

    main

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        deepGreen:"#274C5B",
+        lightGreen:"#274C5B",
+        midGreen:"#7EB693",
+        lightGray:"#F2F0F1"
+      }
+    },
+  },
+  plugins: [],
+}
+
